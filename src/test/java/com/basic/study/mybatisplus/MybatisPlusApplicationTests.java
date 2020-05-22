ab1fc09148153ef10adfc69511131c51e0003393
马誌浩 <mazh@emrubik.com>
2020-05-22 09:22:00 +0800

    #创建mybatis-plus学习项目

---
+++ src/test/java/com/basic/study/mybatisplus/MybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.study.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
